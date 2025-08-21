cc1d67507a1e5ea9a743c2616d32a2e1ab12efae
JessyCastroA <castro.a.jessica@gmail.com>
2025-08-21 13:19:53 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is my portfolio website built with React + Vite.</p>
+    </div>
+  );
+}
+
+export default App;
